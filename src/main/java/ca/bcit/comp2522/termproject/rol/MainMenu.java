a134fc8e95879b8c20409161daa4b8ebb70894a2
JASON_LUI <Jason53lui@gmail.com>
2022-11-18 18:42:14 -0800

    implemented png icons to the map icon buttons

--- src/main/java/ca/bcit/comp2522/termproject/rol/MainMenu.java
+++ src/main/java/ca/bcit/comp2522/termproject/rol/MainMenu.java
@@ -1,14 +1,10 @@
 package ca.bcit.comp2522.termproject.rol;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;

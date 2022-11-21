79b330d3227c684f80b04be4d27eaf28ef5b7905
hailin chen <hchen256@my.bcit.ca>
2022-11-21 13:24:12 -0800

    Deleted unused files

--- src/main/java/ca/bcit/comp2522/termproject/rolDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/rolDriver.java
@@ -1,7 +1,5 @@
 package ca.bcit.comp2522.termproject;
 
-import ca.bcit.comp2522.termproject.rol.SceneManager;
-
 public class rolDriver {
 //    public static void main(final String[] args) {
 //        SceneManager.getMainMenu();
